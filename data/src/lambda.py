799ad7b1136d92f855f5dbfc6a3dc1f96226f259
Jesse Kela <jessekkela@gmail.com>
2024-08-22 18:40:39 +0300

    Invalidate after creating new data

--- data/src/lambda.py
+++ data/src/lambda.py
@@ -2,12 +2,14 @@ import boto3
 import json
 import logging
 import os
+import time
 from pydantic import ValidationError
 from .data_import import get_municipalities, get_vehicles
 from .data_cleaning import clean, generate
 from .data_validation import validate
 
 BUCKET = os.getenv("BUCKET")
+DISTRIBUTION = os.getenv("DISTRIBUTION")
 FILENAME = os.getenv("FILENAME", "data.json")
 
 logger = logging.getLogger()
@@ -29,6 +31,7 @@ def handler(event: dict, context: dict):
         )  # TODO: dynamic date
         valid = validate(data, municipalities)
         if valid:
+            logger.info("Creating new data")
             s3 = boto3.client("s3")
             s3.put_object(
                 Bucket=BUCKET,
@@ -37,7 +40,27 @@ def handler(event: dict, context: dict):
             )
             tags = [{"Key": "AllowExpiration", "Value": "false"}]
             s3.put_object_tagging(Bucket=BUCKET, Key=FILENAME, Tagging={"TagSet": tags})
+            invalidate(FILENAME)
     except ValidationError:
         logger.exception("Invalid data")
     except Exception:
         logger.exception("Error processing data")
+
+
+def invalidate(file: str):
+    try:
+        logger.info("Invalidating data")
+        cf = boto3.client("cloudfront")
+        paths = [f"/{file}"]
+        cf.create_invalidation(
+            DistributionId=DISTRIBUTION,
+            InvalidationBatch={
+                "Paths": {
+                    "Quantity": len(paths),
+                    "Items": paths,
+                },
+                "CallerReference": str(time.time()),
+            },
+        )
+    except Exception:
+        logger.exception("Error invalidating data")
