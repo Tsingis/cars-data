8aa5f645d4c3ddeabef2478894cf3ebcc2c810e2
Jesse Kela <jessekkela@gmail.com>
2024-12-17 19:05:17 +0200

    Code-splitting and lazy loading

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,15 +1,20 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom"
-import HomePage from "./pages/Home/HomePage"
-import ErrorPage from "./pages/Error/ErrorPage"
 import "./app.css"
+import Loading from "./components/Loading/Loading"
+import React, { Suspense } from "react"
+
+const HomePage = React.lazy(() => import("./pages/Home/HomePage"))
+const ErrorPage = React.lazy(() => import("./pages/Error/ErrorPage"))
 
 const App = () => {
   return (
     <Router>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/error" element={<ErrorPage />} />
-      </Routes>
+      <Suspense fallback={<Loading />}>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/error" element={<ErrorPage />} />
+        </Routes>
+      </Suspense>
     </Router>
   )
 }
