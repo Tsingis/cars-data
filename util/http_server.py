a75217b29edaa2df197e99c81d85ddff7ad9f0dd
Jesse Kela <jessekkela@gmail.com>
2025-01-28 00:47:26 +0200

    Update http server util

--- util/http_server.py
+++ util/http_server.py
@@ -4,17 +4,18 @@ import os
 
 PORT = 8000
 
-
-class Handler(http.server.SimpleHTTPRequestHandler):
-    def end_headers(self):
-        self.send_header("Access-Control-Allow-Origin", "*")
-        http.server.SimpleHTTPRequestHandler.end_headers(self)
-
-
 script_dir = os.path.dirname(os.path.abspath(__file__))
 parent_dir = os.path.abspath(os.path.join(script_dir, os.pardir))
 os.chdir(parent_dir)
 
-with socketserver.TCPServer(("", PORT), Handler) as httpd:
-    print(f"Serving at http://localhost:{PORT}")
-    httpd.serve_forever()
+
+class Handler(http.server.SimpleHTTPRequestHandler):
+    def end_headers(self):
+        self.send_header("Access-Control-Allow-Origin", "*")
+        super().end_headers()
+
+
+if __name__ == "__main__":
+    with socketserver.TCPServer(("", PORT), Handler) as httpd:
+        print(f"Serving at http://localhost:{PORT}")
+        httpd.serve_forever()
