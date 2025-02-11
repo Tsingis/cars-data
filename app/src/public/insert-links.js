2c9cda56af1d45a81f76be03af02a4d79ce3db68
Jesse Kela <jessekkela@gmail.com>
2025-02-11 22:15:48 +0200

    Handle insert-links also for localhost

--- app/src/public/insert-links.js
+++ app/src/public/insert-links.js
@@ -1,20 +1,22 @@
 ;(function () {
-  const dataUrlString = "__DATA_URL__"
-
-  if (dataUrlString.trim()) {
-    const dataUrl = new URL(dataUrlString)
-    const head = document.head
-
-    const dnsPrefectLink = document.createElement("link")
-    dnsPrefectLink.rel = "dns-prefetch"
-    dnsPrefectLink.href = dataUrl.origin
-    head.appendChild(dnsPrefectLink)
-
-    const preloadLink = document.createElement("link")
-    preloadLink.rel = "preload"
-    preloadLink.href = dataUrl
-    preloadLink.as = "fetch"
-    preloadLink.crossOrigin = "anonymous"
-    head.appendChild(preloadLink)
+  let dataUrl
+  try {
+    dataUrl = new URL("__DATA_URL__")
+  } catch {
+    dataUrl = new URL("http://localhost:8000/data/data.json")
   }
+
+  const head = document.head
+
+  const dnsPrefetchLink = document.createElement("link")
+  dnsPrefetchLink.rel = "dns-prefetch"
+  dnsPrefetchLink.href = dataUrl.origin
+  head.appendChild(dnsPrefetchLink)
+
+  const preloadLink = document.createElement("link")
+  preloadLink.rel = "preload"
+  preloadLink.href = dataUrl
+  preloadLink.as = "fetch"
+  preloadLink.crossOrigin = "anonymous"
+  head.appendChild(preloadLink)
 })()
