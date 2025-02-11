2f5c591b7be306573534d41e0675a46d09671f4d
Jesse Kela <jessekkela@gmail.com>
2025-02-11 22:17:31 +0200

    Embed typechecking for dev server

--- app/config/vite.config.ts
+++ app/config/vite.config.ts
@@ -1,8 +1,25 @@
 import { defineConfig } from "vite"
 import react from "@vitejs/plugin-react"
+import checker from "vite-plugin-checker"
+
+const args = process.argv.slice(3)
+const useChecker = args.includes("--use-checker")
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    useChecker &&
+      checker({
+        typescript: {
+          tsconfigPath: "./config/tsconfig.json",
+        },
+        //TODO: Test once eslint v9 flatfile config is supported
+        // eslint: {
+        //   lintCommand: "eslint . --config ./config/eslint.config.js",
+        //   useFlatConfig: true,
+        // },
+      }),
+  ],
   root: "src",
   publicDir: "public",
   envDir: "..",
