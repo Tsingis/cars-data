496a023f52c331c74b594934f28db93f92f9553d
Jesse Kela <jessekkela@gmail.com>
2025-02-23 23:59:49 +0200

    Implement dark/light theme switch

--- app/src/components/NavBar/NavBar.tsx
+++ app/src/components/NavBar/NavBar.tsx
@@ -1,12 +1,14 @@
 import React from "react"
 import LanguageSwitch from "../LanguageSwitch/LanguageSwitch"
 import "./NavBar.modules.css"
+import ThemeSwitch from "../ThemeSwitch/ThemeSwitch"
 
 const NavBar = () => {
   return (
     <nav className="navbar">
       <div className="navbar-items">
         <LanguageSwitch />
+        <ThemeSwitch />
       </div>
     </nav>
   )
---
+++ app/src/components/ThemeSwitch/ThemeSwitch.tsx
@@ -0,0 +1,38 @@
+import React, { useState, useEffect } from "react"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faSun, faMoon } from "@fortawesome/free-solid-svg-icons"
+import "./ThemeSwitch.modules.css"
+
+const ThemeSwitch = () => {
+  const [theme, setTheme] = useState(
+    () => localStorage.getItem("theme") || "light"
+  )
+
+  useEffect(() => {
+    document.documentElement.setAttribute("data-theme", theme)
+    localStorage.setItem("theme", theme)
+  }, [theme])
+
+  const changeTheme = (newTheme: string) => {
+    setTheme(newTheme)
+  }
+
+  return (
+    <div className="theme-switch-container">
+      <button
+        className={`theme-switch-button ${theme === "dark" ? "active" : ""}`}
+        onClick={() => changeTheme("dark")}
+      >
+        <FontAwesomeIcon icon={faMoon} />
+      </button>
+      <button
+        className={`theme-switch-button ${theme === "light" ? "active" : ""}`}
+        onClick={() => changeTheme("light")}
+      >
+        <FontAwesomeIcon icon={faSun} />
+      </button>
+    </div>
+  )
+}
+
+export default ThemeSwitch
