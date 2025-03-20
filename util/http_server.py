99d385d9da6d088d78bd8169228ffebc65382755
Jesse Kela <jessekkela@gmail.com>
2025-03-20 16:32:30 +0200

    Setup test http server in container

--- util/http_server.py
+++ util/http_server.py
@@ -4,10 +4,6 @@ import os
 
 PORT = 8000
 
-script_dir = os.path.dirname(os.path.abspath(__file__))
-parent_dir = os.path.abspath(os.path.join(script_dir, os.pardir))
-os.chdir(parent_dir)
-
 
 class Handler(http.server.SimpleHTTPRequestHandler):
     def end_headers(self):
@@ -16,6 +12,9 @@ class Handler(http.server.SimpleHTTPRequestHandler):
 
 
 if __name__ == "__main__":
+    script_dir = os.path.dirname(os.path.abspath(__file__))
+    parent_dir = os.path.abspath(os.path.join(script_dir, os.pardir))
+    os.chdir(parent_dir)
     with socketserver.TCPServer(("", PORT), Handler) as httpd:
         print(f"Serving at http://localhost:{PORT}")
         httpd.serve_forever()
