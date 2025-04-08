d225ff9c3d0361847998c62175c6bb9f53f7f231
Jesse Kela <jessekkela@gmail.com>
2025-04-08 11:10:48 +0300

    Add i18n json eslint plugin

--- app/eslint.config.js
+++ app/eslint.config.js
@@ -1,13 +1,34 @@
+import path from "path"
 import js from "@eslint/js"
 import parser from "@typescript-eslint/parser"
 import react from "eslint-plugin-react"
 import hooks from "eslint-plugin-react-hooks"
 import refresh from "eslint-plugin-react-refresh"
 import i18next from "eslint-plugin-i18next"
+import i18nJsonPlugin from "eslint-plugin-i18n-json"
 
 export default [
   js.configs.recommended,
   i18next.configs["flat/recommended"],
+  {
+    files: ["src/i18n/**/*.json"],
+
+    plugins: { "i18n-json": i18nJsonPlugin },
+    processor: {
+      meta: { name: ".json" },
+      ...i18nJsonPlugin.processors[".json"],
+    },
+    rules: {
+      ...i18nJsonPlugin.configs.recommended.rules,
+      "i18n-json/valid-message-syntax": "off",
+      "i18n-json/identical-keys": [
+        "error",
+        {
+          filePath: path.resolve("src/i18n/en.json"),
+        },
+      ],
+    },
+  },
   {
     files: ["**/*.{js,jsx,ts,tsx}"],
     languageOptions: {
