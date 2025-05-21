887e3e8d9b6517533f7d132a210d9564d3624017
Jesse Kela <jessekkela@gmail.com>
2025-05-21 23:24:35 +0300

    Improve http test server stability and concurrency

--- util/http_server.py
+++ util/http_server.py
@@ -4,6 +4,8 @@ import os
 
 PORT = 8000
 
+socketserver.ThreadingTCPServer.allow_reuse_address = True
+
 
 class Handler(http.server.SimpleHTTPRequestHandler):
     def end_headers(self):
@@ -15,6 +17,11 @@ if __name__ == "__main__":
     script_dir = os.path.dirname(os.path.abspath(__file__))
     parent_dir = os.path.abspath(os.path.join(script_dir, os.pardir))
     os.chdir(parent_dir)
-    with socketserver.TCPServer(("", PORT), Handler) as httpd:
+    with socketserver.ThreadingTCPServer(("127.0.0.1", PORT), Handler) as httpd:
+        httpd.timeout = 60
         print(f"Serving at http://localhost:{PORT}")
-        httpd.serve_forever()
+        try:
+            httpd.serve_forever()
+        except KeyboardInterrupt:
+            print("\nShutting down server.")
+            httpd.server_close()
