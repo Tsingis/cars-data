875e99e4b5a7d8c5ae22749760f11aa88a83b226
Jesse Kela <jessekkela@gmail.com>
2025-05-24 23:39:17 +0300

    Improved python http server

--- util/http_server.py
+++ util/http_server.py
@@ -1,6 +1,5 @@
 import http.server
 import socketserver
-import os
 
 PORT = 8000
 
@@ -8,16 +7,18 @@ socketserver.ThreadingTCPServer.allow_reuse_address = True
 
 
 class Handler(http.server.SimpleHTTPRequestHandler):
+    def do_GET(self):
+        if not self.path.startswith("/data/"):
+            self.path = "/data" + self.path
+        return super().do_GET()
+
     def end_headers(self):
         self.send_header("Access-Control-Allow-Origin", "*")
         super().end_headers()
 
 
 if __name__ == "__main__":
-    script_dir = os.path.dirname(os.path.abspath(__file__))
-    parent_dir = os.path.abspath(os.path.join(script_dir, os.pardir))
-    os.chdir(parent_dir)
-    with socketserver.ThreadingTCPServer(("127.0.0.1", PORT), Handler) as httpd:
+    with socketserver.ThreadingTCPServer(("0.0.0.0", PORT), Handler) as httpd:
         httpd.timeout = 60
         print(f"Serving at http://localhost:{PORT}")
         try:
