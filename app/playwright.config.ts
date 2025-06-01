9461302fe259fe4b9bdbe222457702b1234e851e
Jesse Kela <jessekkela@gmail.com>
2025-06-01 22:18:33 +0300

    Lower timeouts in playwright

--- app/playwright.config.ts
+++ app/playwright.config.ts
@@ -1,14 +1,16 @@
-import { defineConfig, devices } from "@playwright/test"
+import { defineConfig, devices, type ViewportSize } from "@playwright/test"
+
+const viewPort: ViewportSize = { width: 1280, height: 800 }
 
 export default defineConfig({
   testDir: "./tests/playwright",
   fullyParallel: true,
   reporter: "list",
-  timeout: 180_000,
+  timeout: 60_000,
   retries: 3,
   workers: process.env.CI ? 1 : undefined,
   expect: {
-    timeout: 30_000,
+    timeout: 10_000,
     toHaveScreenshot: {
       maxDiffPixelRatio: Number(process.env.MAX_DIFF_PIXEL_RATIO) || 0,
       animations: "disabled",
@@ -20,7 +22,7 @@ export default defineConfig({
       reuseExistingServer: !process.env.CI,
       stdout: "ignore",
       stderr: "ignore",
-      timeout: 60_000,
+      timeout: 30_000,
       url: "http://localhost:8000",
     },
     {
@@ -28,7 +30,7 @@ export default defineConfig({
       reuseExistingServer: !process.env.CI,
       stdout: "ignore",
       stderr: "pipe",
-      timeout: 60_000,
+      timeout: 30_000,
       url: "http://localhost:3000",
     },
   ],
@@ -37,7 +39,7 @@ export default defineConfig({
       name: "firefox",
       use: {
         ...devices["Desktop Firefox"],
-        viewport: { width: 1280, height: 800 },
+        viewport: viewPort,
         screenshot: "off",
         video: "off",
         trace: "off",
@@ -47,7 +49,7 @@ export default defineConfig({
       name: "webkit",
       use: {
         ...devices["Desktop Safari"],
-        viewport: { width: 1280, height: 800 },
+        viewport: viewPort,
         screenshot: "off",
         video: "off",
         trace: "off",
@@ -57,7 +59,7 @@ export default defineConfig({
       name: "chromium",
       use: {
         ...devices["Desktop Chrome"],
-        viewport: { width: 1280, height: 800 },
+        viewport: viewPort,
         screenshot: "off",
         video: "off",
         trace: "off",
