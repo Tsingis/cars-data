cd983eb95439d972a86acada5a02b6c8f6f6c6f2
Jesse Kela <jessekkela@gmail.com>
2025-07-23 13:29:40 +0300

    Add biome to checker

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -17,6 +17,9 @@ export default defineConfig({
           lintCommand: "eslint . --config ./eslint.config.js",
           useFlatConfig: true,
         },
+        biome: {
+          command: "check",
+        },
       }),
   ],
   root: "src",
