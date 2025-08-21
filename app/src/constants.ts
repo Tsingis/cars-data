f4bcc633f77e3452e18c887ce2b295256b544d3b
Jesse Kela <jessekkela@gmail.com>
2025-08-21 11:22:14 +0300

    Removing obsolete code

--- app/src/constants.ts
+++ app/src/constants.ts
@@ -13,14 +13,6 @@ export const drivingForceColors: Mapping = {
   other: "rgba(93, 93, 93, 0.6)",
 };
 
-export const drivingForceLabels: Mapping = {
-  petrol: "DrivingForces.Petrol",
-  diesel: "DrivingForces.Diesel",
-  hybrid: "DrivingForces.Hybrid",
-  electricity: "DrivingForces.Electricity",
-  other: "DrivingForces.Other",
-};
-
 export const colors: Mapping = {
   black: "rgba(0, 0, 0, 0.6)",
   blue: "rgba(0, 123, 255, 0.6)",
@@ -31,16 +23,4 @@ export const colors: Mapping = {
   silver: "rgba(170, 169, 173, 0.6)",
   white: "rgba(255, 255, 255, 0.9)",
   other: "rgba(255, 193, 7, 0.6)",
-};
-
-export const colorLabels: Mapping = {
-  black: "Colors.Black",
-  blue: "Colors.Blue",
-  brown: "Colors.Brown",
-  green: "Colors.Green",
-  grey: "Colors.Grey",
-  red: "Colors.Red",
-  silver: "Colors.Silver",
-  white: "Colors.White",
-  other: "Colors.Other",
 };
